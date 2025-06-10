aef3b9f198c7c8220a8a9bd7d090528ed368eadd
Beau Ginbey <your-email@example.com>
2025-06-11 03:47:57 +0800

    [Refactor] Remove temporary test files and update .gitignore

--- run-hl.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Set up environment for HashLink
-export DYLD_LIBRARY_PATH=".:$DYLD_LIBRARY_PATH"
-export DYLD_FALLBACK_LIBRARY_PATH=".:$DYLD_FALLBACK_LIBRARY_PATH"
-
-# Run HashLink
-./hl "$@"
